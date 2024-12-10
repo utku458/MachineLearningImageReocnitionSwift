2e946da40eb8709f1c43664721ba1a081f802d4d
utku <altinayutku0@gmail.com>
2024-12-10 20:02:52 +0300

    Initial Commit

---
+++ MachineLearningImageReocnition/ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  MachineLearningImageReocnition
+//
+//  Created by Utku Altınay on 7.12.2024.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    var chosenImage = CIImage()
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    
+
+    @IBAction func changeClicked(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true)
+        if let ciImage = CIImage(image: imageView.image!){
+            chosenImage = ciImage
+            
+        }
+        recognizeImage(image: chosenImage)
+    }
+    
+    
+    func recognizeImage(image:CIImage){
+        if let model = try? VNCoreMLModel(for: MobileNetV2().model){
+            
+            let request = VNCoreMLRequest(model: model) { vnrequest, error in
+                if let results = vnrequest.results as? [VNClassificationObservation]{
+                    if results.count > 0 {
+                        let topResult = results.first
+                        
+                        DispatchQueue.main.async {
+                            
+                            let  confidenceLevel = (topResult?.confidence ?? 0) * 100
+                            let rounded = Int(confidenceLevel) / 100
+                            self.resultLabel.text = "\(confidenceLevel)% it's \(topResult?.identifier ?? "nil")"
+                        }
+                    }
+                }
+            }
+            let handler = VNImageRequestHandler(ciImage: image)
+            
+            DispatchQueue.global(qos: .userInteractive).async {
+                do {
+                    try handler.perform([request])
+                } catch{
+                    print("error")
+                }
+            }
+        }
+        
+      
+    }
+    
+}
+
